88803ff186f913f52649eb67e04e61f177489c91
Viswaksena Jayam <viswaksene111@gmail.com>
2024-03-13 19:30:24 +0530

    Initial commit

---
+++ backend/models/Rooms.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const roomSchema = new mongoose.Schema({
+    roomNumber: { type: String, required: true, unique: true },
+    roomType: { type: String, required: true },
+    pricePerHour: { type: Number, required: true },
+  },{
+    timestamps: true,
+  });
+
+module.exports = mongoose.model("Room", roomSchema);
